fa9d2f1e3e0a190af66376fd39ef27d976dc6c11
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-08 16:09:35 +0300

    ui shopping cart component handled

--- Frontends/MultiShop.WebUi/Components/Ui Shopping Cart Components/_UiShoppingCartProductListComponent.cs
+++ Frontends/MultiShop.WebUi/Components/Ui Shopping Cart Components/_UiShoppingCartProductListComponent.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUi.Services.BasketServices;
 
 namespace MultiShop.WebUi.Components.Ui_ShoppingCart_Components
 {
     public class _UiShoppingCartProductListComponent : ViewComponent
     {
+        private readonly IBasketService _basketService;
+
+        public _UiShoppingCartProductListComponent(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var basketTotal = await _basketService.GetBasket();
+            var basketItems = basketTotal.BasketItems;
+            return View(basketItems);
         }
     }
 }
