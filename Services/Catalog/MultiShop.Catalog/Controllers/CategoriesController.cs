b289c071c95b11efe95a31dc5c2069c6af6514d6
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-08 16:12:55 +0300

    categories controller updated

--- Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Services.CategoryServices;
 
