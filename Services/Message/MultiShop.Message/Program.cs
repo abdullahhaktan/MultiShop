d21f71957e27075221a508efccb248fed22eb3d4
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-13 16:36:07 +0300

    MultiShop.Message project has been created

--- Services/Message/MultiShop.Message/Program.cs
+++ Services/Message/MultiShop.Message/Program.cs
@@ -1,9 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Context;
+using MultiShop.Message.Services;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MessageContext>(opt =>
+{
+    opt.UseNpgsql(connectionString);
+});
+
+builder.Services.AddScoped<IUserMessageService,UserMessageService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
