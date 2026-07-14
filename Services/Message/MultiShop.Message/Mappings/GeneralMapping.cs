1be2b978baa216fa73e7dea6a394ba8562c2ef5b
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-14 16:11:32 +0300

    message service api operations handled

---
+++ Services/Message/MultiShop.Message/Mappings/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MultiShop.Message.DAL.Entites;
+using MultiShop.Message.DTOs;
+
+namespace MultiShop.Message.Mappings
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<UserMessage, ResultMessageDto>().ReverseMap();
+            CreateMap<UserMessage, CreateMessageDto>().ReverseMap();
+            CreateMap<UserMessage, UpdateMessageDto>().ReverseMap();
+            CreateMap<UserMessage, ResultInboxMessageDto>().ReverseMap();
+            CreateMap<UserMessage, ResultSendboxMessageDto>().ReverseMap();
+            CreateMap<UserMessage, GetMessageByIdDto>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Message/MultiShop.Message/Services/IUserMessageService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Message.DTOs;
+
+namespace MultiShop.Message.Services
+{
+    public interface IUserMessageService
+    {
+        Task<List<ResultMessageDto>> GetAllMessageAsync();
+        Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id);
+        Task<List<ResultSendboxMessageDto>> GetSendboxMessageAsync(string id);
+        Task CreateMessageAsync(CreateMessageDto createMessageDto);
+        Task UpdateMessageAsync(UpdateMessageDto updateMessageDto);
+        Task DeleteMessageAsync(int id);
+        Task<GetMessageByIdDto> GetMessageByIdAsync(int id);
+
+    }
+}
