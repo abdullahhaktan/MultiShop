07218fbd46dcf5efb03935c6bef259806d1716f4
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-14 16:19:06 +0300

    message microservice , getting sended and received messages operations has been handled

---
+++ Frontends/MultiShop.WebUi/Services/MessageServices/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using MultiShop.DtoLayer.MessageDtos;
+
+namespace MultiShop.WebUi.Services.MessageServices
+{
+    public interface IMessageService
+    {
+        Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id);
+        Task<List<ResultSendboxMessageDto>> GetSendboxMessageAsync(string id);
+    }
+}
---
+++ Frontends/MultiShop.WebUi/Services/MessageServices/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using MultiShop.DtoLayer.MessageDtos;
+
+namespace MultiShop.WebUi.Services.MessageServices
+{
+    public class MessageService : IMessageService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MessageService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<ResultInboxMessageDto>> GetInboxMessageAsync(string id)
+        {
+            var responseMessage = await _httpClient.GetAsync($"userMessages/getMessageInbox/{id}");
+            var values = await responseMessage.Content.ReadFromJsonAsync<List<ResultInboxMessageDto>>();
+            return values;
+        }
+
+        public async Task<List<ResultSendboxMessageDto>> GetSendboxMessageAsync(string id)
+        {
+            var responseMessage = await _httpClient.GetAsync($"userMessages/getMessageSendbox/{id}");
+            var values = await responseMessage.Content.ReadFromJsonAsync<List<ResultSendboxMessageDto>>();
+            return values;
+        }
+
+    }
+}
