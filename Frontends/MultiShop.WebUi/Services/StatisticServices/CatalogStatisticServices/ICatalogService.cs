9934392299c4b11ad99bc0bccff79fd2e2a7a685
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-16 18:03:46 +0300

    statistic controller has been initiliazed

---
+++ Frontends/MultiShop.WebUi/Services/StatisticServices/CatalogStatisticServices/ICatalogService.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUi.Services.StatisticServices.CatalogStatisticServices
+{
+    public interface ICatalogService
+    {
+        Task<long> GetCategoryCount();
+        Task<long> GetProductCount();
+        Task<long> GetBrandCount();
+        Task<decimal> GetProductAvgPrice();
+        Task<string> GetMaxPriceProductName();
+        Task<string> GetMinPriceProductName();
+    }
+}
--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -12,6 +12,7 @@ using MultiShop.Catalog.Services.ProductDetailServices;
 using MultiShop.Catalog.Services.ProductImageServices;
 using MultiShop.Catalog.Services.ProductServices;
 using MultiShop.Catalog.Services.SpecialOfferServices;
+using MultiShop.Catalog.Services.StatisticServices;
 using MultiShop.Catalog.Settings;
 using System.Reflection;
 
@@ -31,6 +32,7 @@ builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
 builder.Services.AddScoped<IProductImageService, ProductImageService>();
 builder.Services.AddScoped<IFeatureSliderService, FeatureSliderService>();
 builder.Services.AddScoped<ISpecialOfferService, SpecialOfferService>();
+builder.Services.AddScoped<IStatisticService, StatisticService>();
 builder.Services.AddScoped<IFeatureService, FeatureService>();
 builder.Services.AddScoped<IFeaturedProductService, FeaturedProductService>();
 builder.Services.AddScoped<IBrandService, BrandService>();
