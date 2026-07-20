bcb66737f0a5e91a091d8d875df3a1f406cfea73
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-20 17:13:28 +0300

    signalR class created

---
+++ Services/SignalRRealTime/MultiShop.SignalRRealTime/Hubs/SignalRHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MultiShop.SignalRRealTime.Services.SignalRCommentServices;
+using MultiShop.SignalRRealTime.Services.SignaRMessageServices;
+
+namespace MultiShop.SignalRRealTime.Hubs
+{
+    public class SignalRHub : Hub
+    {
+        private readonly ISignalRCommentService _signalRCommentService;
+        private readonly ISignalRMessageService _signalRMessageService;
+
+        public SignalRHub(ISignalRCommentService signalRCommentService, ISignalRMessageService signalRMessageService)
+        {
+            _signalRCommentService = signalRCommentService;
+            _signalRMessageService = signalRMessageService;
+        }
+
+        //public async Task SendStatisticCount
+
+    }
+}
