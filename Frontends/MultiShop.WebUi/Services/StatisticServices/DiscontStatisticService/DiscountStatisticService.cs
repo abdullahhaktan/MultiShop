d8aabcd3a032930e234e22aebd6429f742ff150d
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-20 17:35:05 +0300

    discount ui services completed

---
+++ Frontends/MultiShop.WebUi/Services/StatisticServices/DiscontStatisticService/DiscountStatisticService.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MultiShop.WebUi.Services.StatisticServices.DiscontStatisticService
+{
+    public class DiscountStatisticService : IDiscountStatisticService
+    {
+        private readonly HttpClient _httpClient;
+        public DiscountStatisticService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<int> GetDiscountCouponCountAsync()
+        {
+            var responseMessage = await _httpClient.GetAsync("DiscountCoupons/GetDiscountCouponCount");
+            var values = await responseMessage.Content.ReadFromJsonAsync<int>();
+            return values;
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUi/Services/StatisticServices/DiscontStatisticService/IDiscountStatisticService.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.WebUi.Services.StatisticServices.DiscontStatisticService
+{
+    public interface IDiscountStatisticService
+    {
+        Task<int> GetDiscountCouponCountAsync();
+    }
+}
