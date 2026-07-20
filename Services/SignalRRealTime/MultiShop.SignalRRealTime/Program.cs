9359a70d4e7c5e6ca27dd9cf163ffc313d50a282
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-20 18:00:15 +0300

    Cors policy configuration added

--- Services/SignalRRealTime/MultiShop.SignalRRealTime/Program.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTime/Program.cs
@@ -1,15 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader()
+               .AllowAnyMethod()
+               .SetIsOriginAllowed((host) => true)
+               .AllowCredentials();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
