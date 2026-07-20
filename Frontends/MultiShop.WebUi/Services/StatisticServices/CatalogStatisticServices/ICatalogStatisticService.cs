8161a85dce55d1af031612ced2103004019ec43f
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-20 13:45:42 +0300

    catalog service api services renamed

---
+++ Frontends/MultiShop.WebUi/Services/StatisticServices/CatalogStatisticServices/ICatalogStatisticService.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUi.Services.StatisticServices.CatalogStatisticServices
+{
+    public interface ICatalogStatisticService
+    {
+        Task<long> GetCategoryCountAsync();
+        Task<long> GetProductCountAsync();
+        Task<long> GetBrandCountAsync();
+        Task<decimal> GetProductAvgPriceAsync();
+        Task<string> GetMinPriceProductNameAsync();
+        Task<string> GetMaxPriceProductNameAsync();
+    }
+}
