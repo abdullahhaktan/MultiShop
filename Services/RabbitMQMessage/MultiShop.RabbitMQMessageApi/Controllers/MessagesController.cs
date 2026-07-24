48a8fb75fc546bdc2ac7f41594f2e845d81d55c3
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-24 15:59:14 +0300

    creating message on rabbitmq message controller completed

--- Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace MultiShop.RabbitMQMessageApi.Controllers
 {
@@ -7,9 +9,25 @@ namespace MultiShop.RabbitMQMessageApi.Controllers
     [ApiController]
     public class MessagesController : ControllerBase
     {
-        [HttpGet]
+        [HttpPost]
         public async Task<IActionResult> CreateMessage()
         {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using var connection = await connectionFactory.CreateConnectionAsync(); // using eklendi
+            using var channel = await connection.CreateChannelAsync(); // using eklendi
+
+            await channel.QueueDeclareAsync("Kuyruk2",false,false,false,arguments:null);
+
+            var messageContent = "Merhaba bu bir rabbitmq kuyruk test mesajıdır";
+
+            var byteMessageContent = Encoding.UTF8.GetBytes(messageContent);
+
+            await channel.BasicPublishAsync(exchange: "", routingKey: "Kuyruk2", mandatory:true, basicProperties: new BasicProperties(), body: byteMessageContent);
+
             return Ok("Mesajınız kuyruğa alınmıştır");
         }
     }
