c062dad5d14bcc7c606933f11729fdd943d4cd56
ABDULLAH HAKTAN <abdullahhaktan9@gmail.com>
2026-07-24 11:49:22 +0300

    RapidApi project and RabbitMQMessage project have been initialized

---
+++ Services/RabbitMQMessage/MultiShop.RabbitMQMessageApi/Controllers/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.RabbitMQMessageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> CreateMessage()
+        {
+            return Ok("Mesajınız kuyruğa alınmıştır");
+        }
+    }
+}
